703891c6b794383e935d92ffa3e158491f6ce269
Romary Dupuis <romary@me.com>
2018-11-16 23:50:45 +0100

    Add hello world with templates example

---
+++ examples/hello-world/hello_world_template.py
@@ -0,0 +1,57 @@
+from lifoid import Lifoid
+from lifoid.action import action
+from lifoid.message import LifoidMessage, Payload
+from lifoid.message.message_types import CHAT
+from lifoid.renderer import Renderer
+from lifoid.views import render_view
+
+
+LIFOID_ID = 'hello_world'
+
+
+class HelloWorldRenderer(Renderer):
+    """
+    Stdout renderer
+    """
+    def render(self, messages, receiver_id):
+        for message in self.convert(messages):
+            print('--> {}'.format(message.payload))
+
+    def convert(self, messages):
+        return messages
+
+
+@action(lambda message, context: 'Bob' in message.payload.text and
+        message.message_type == CHAT)
+def hello_bob(render, _message, _context):
+    """
+    Simply say hello
+    """
+    return render_view(render, 'hello.yml', context={'name': 'Bob'})
+
+
+@action(lambda message, context: 'John' in message.payload.text and
+        message.message_type == CHAT)
+def hello_john(render, _message, _context):
+    """
+    Simply say hello
+    """
+    return render_view(render, 'hello.txt', context={'name': 'John'})
+
+
+lifoid = Lifoid(LIFOID_ID, actions=[hello_bob, hello_john],
+                renderer=HelloWorldRenderer())
+
+resp = lifoid.reply(
+    LifoidMessage(
+        payload=Payload(text='I am Bob'),
+        message_type=CHAT
+    )
+)
+
+esp = lifoid.reply(
+    LifoidMessage(
+        payload=Payload(text='I am John'),
+        message_type=CHAT
+    )
+)
