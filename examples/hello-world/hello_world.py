79e20f448da7ecbb63fa36a2f9ecb459425e53f2
Romary Dupuis <romary@me.com>
2018-11-16 23:07:39 +0100

    Hello World example

---
+++ examples/hello-world/hello_world.py
@@ -0,0 +1,49 @@
+import os
+from lifoid import Lifoid
+from lifoid.action import action
+from lifoid.message import LifoidMessage, Payload
+from lifoid.message.message_types import CHAT
+from lifoid.renderer import Renderer
+
+
+LIFOID_ID = 'hello_world'
+
+
+class HelloWorldRenderer(Renderer):
+    """
+    Stdout renderer
+    """
+    def render(self, messages, receiver_id):
+        for message in self.convert(messages):
+            print('--> {}'.format(message))
+
+    def convert(self, messages):
+        return messages
+
+
+@action(lambda message, context: 'hello' in message.payload and
+        message.message_type == CHAT)
+def hello_world(render, _message, _context):
+    """
+    Simply say hello
+    """
+    return render([
+        LifoidMessage(
+            message_type=CHAT,
+            payload='hello'
+        )
+    ])
+
+
+lifoid = Lifoid(LIFOID_ID, actions=[hello_world],
+                renderer=HelloWorldRenderer())
+
+message = LifoidMessage(
+    payload=Payload(text='hello'),
+    message_type=CHAT
+)
+
+resp = lifoid.reply(
+    message
+)
+print(resp)
