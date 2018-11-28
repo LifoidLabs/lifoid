754eecd637e5f7984a3b50cd809c4d280b7a88f9
Romary <romary@me.com>
2018-11-28 15:36:24 +0100

    Test IoT renderer

--- lifoid/webhook/renderer.py
+++ lifoid/webhook/renderer.py
@@ -3,9 +3,13 @@ Special CLI oriented renderer for talk command
 Author:   Romary Dupuis <romary@me.com>
 Copyright (C) 2017-2018-2018 Romary Dupuis
 """
+import boto3
 from lifoid.renderer import Renderer
 from loggingmixin import LoggingMixin
 from lifoid.utils.asdict import namedtuple_asdict
+
+
+IOT_CLIENT = boto3.client('iot-data', region_name='eu-west-1')
 
 
 class Renderer(Renderer, LoggingMixin):
@@ -20,5 +24,10 @@ class Renderer(Renderer, LoggingMixin):
     def render(self, messages, receiver_id):
         for msg in self.convert(messages):
             import json
+            response = IOT_CLIENT.publish(
+                topic='multibot/{}'.format(receiver_id),
+                qos=1,
+                payload=json.dumps(namedtuple_asdict(msg))
+            )
             self.logger.debug(
                 'Response: {}'.format(json.dumps(namedtuple_asdict(msg))))
